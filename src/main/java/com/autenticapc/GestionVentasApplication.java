5575f10ff3ae8d2c6065d5c9d0109e42b9c67412
jazamora-rosabal <jazamora.rosabal@gmail.com>
2020-05-13 17:55:18 -0400

    Creando el proyecto base con todas las dependencias

---
+++ src/main/java/com/autenticapc/GestionVentasApplication.java
@@ -0,0 +1,13 @@
+package com.autenticapc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionVentasApplication.class, args);
+	}
+
+}
